2d18b5ad15e746b602c3105240d779342a1689fa
git <your_email@example.com>
2019-03-15 10:25:39 +0700

    update

--- bin/setenv.sh
+++
@@ -1,9 +0,0 @@
-export JAVA_OPTS = "-Dcom.sun.management.jmxremote \
-					-Dcom.sun.management.jmxremote.port=7070 \
-					-Dcom.sun.management.jmxremote.authenticate=false \
-					-Dcom.sun.management.jmxremote.ssl=false \
-					-Djava.rmi.server.hostname=192.168.168.115 \
-					-Djdk.tls.ephemeralDHKeySize=2048 -Xmx2048m \
-					-XX:+UseConcMarkSweepGC \
-					-XX:+CMSClassUnloadingEnabled \
-					-server "
